779e22801fc70998c6b2d1c6bd6390a90ba4237b
Ilya Mizus <bigfatbrowncat@gmail.com>
2013-09-06 05:32:01 +0400

    Prepared Eclipse project converted to a template

--- zetes/ide-conf/ide-conf.mk
+++ zetes/ide-conf/ide-conf.mk
@@ -6,22 +6,33 @@ ifndef ARCH
 endif
 
 ifeq ($(UNAME), Darwin)	# OS X
+  PLATFORM_SHORT= osx
   PLATFORM_TAG = osx-x86_64
 else ifeq ($(UNAME) $(ARCH), Linux x86_64)	# linux on PC
+  PLATFORM_SHORT= linux
   PLATFORM_TAG = linux-x86_64
 else ifeq ($(UNAME) $(ARCH), Linux armv6l)	# linux on Raspberry Pi
+  PLATFORM_SHORT= linux
   PLATFORM_TAG = linux-armv6l
 else ifeq ($(OS) $(ARCH), Windows_NT i686)	# Windows
+  PLATFORM_SHORT= windows
   PLATFORM_TAG = win-i386
 else ifeq ($(OS) $(ARCH), Windows_NT x86_64)	# Windows
+  PLATFORM_SHORT= windows
   PLATFORM_TAG = win-x86_64
 endif
 
+VARS_SUBST='s|<ARCH>|$(ARCH)|g;s|<JAVA_HOME>|$(JAVA_HOME)|g'
+IDE_CONF_SRC=$(IDE_CONF)/$(PLATFORM_TAG)
+
 ide-conf-eclipse:
 	@echo Extracting Eclipse project files for $(PLATFORM_TAG)...
-	cp -f $(IDE_CONF)/$(PLATFORM_TAG)/eclipse/.project ./
-	cp -f $(IDE_CONF)/$(PLATFORM_TAG)/eclipse/.cproject ./
-	cp -f $(IDE_CONF)/$(PLATFORM_TAG)/eclipse/.classpath ./
-	cp -rf $(IDE_CONF)/$(PLATFORM_TAG)/eclipse/.settings ./
+	mkdir -p .settings
+	rm -f .settings/org.eclipse.cdt.core.prefs
+	sed $(VARS_SUBST) < $(IDE_CONF)/eclipse/settings/org.eclipse.cdt.core.prefs.template > .settings/org.eclipse.cdt.core.prefs
+	sed $(VARS_SUBST) < $(IDE_CONF)/eclipse/classpath.template > .classpath
+	sed $(VARS_SUBST) < $(IDE_CONF)/eclipse/project.template > .project
+	sed $(VARS_SUBST) < $(IDE_CONF)/eclipse/$(PLATFORM_SHORT)/cproject.template > .cproject
+
 
 .SILENT:

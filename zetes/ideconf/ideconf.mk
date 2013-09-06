27cdd4dfb32ec931dcc49cf78d23afebf2de4006
Ilya Mizus <bigfatbrowncat@gmail.com>
2013-09-06 11:12:09 +0400

    Build utility help added

--- zetes/ideconf/ideconf.mk
+++ zetes/ideconf/ideconf.mk
@@ -1,4 +1,4 @@
-IDE_CONF=ide-conf
+IDE_CONF=ideconf
 
 UNAME := $(shell uname)
 ifndef ARCH
@@ -6,26 +6,26 @@ ifndef ARCH
 endif
 
 ifeq ($(UNAME), Darwin)	# OS X
-  PLATFORM_SHORT= osx
+  PLATFORM_SHORT = osx
   PLATFORM_TAG = osx-x86_64
 else ifeq ($(UNAME) $(ARCH), Linux x86_64)	# linux on PC
-  PLATFORM_SHORT= linux
+  PLATFORM_SHORT = linux
   PLATFORM_TAG = linux-x86_64
 else ifeq ($(UNAME) $(ARCH), Linux armv6l)	# linux on Raspberry Pi
-  PLATFORM_SHORT= linux
+  PLATFORM_SHORT = linux
   PLATFORM_TAG = linux-armv6l
 else ifeq ($(OS) $(ARCH), Windows_NT i686)	# Windows
-  PLATFORM_SHORT= windows
+  PLATFORM_SHORT = win
   PLATFORM_TAG = win-i386
 else ifeq ($(OS) $(ARCH), Windows_NT x86_64)	# Windows
-  PLATFORM_SHORT= windows
+  PLATFORM_SHORT = win
   PLATFORM_TAG = win-x86_64
 endif
 
-VARS_SUBST='s|<ARCH>|$(ARCH)|g;s|<JAVA_HOME>|$(JAVA_HOME)|g'
+VARS_SUBST='s|<ARCH>|$(ARCH)|g;s|<PLATFORM_SHORT>|$(PLATFORM_SHORT)|g;s|<JAVA_HOME>|$(JAVA_HOME)|g'
 IDE_CONF_SRC=$(IDE_CONF)/$(PLATFORM_TAG)
 
-ide-conf-eclipse:
+ideconf-eclipse:
 	@echo Extracting Eclipse project files for $(PLATFORM_TAG)...
 	mkdir -p .settings
 	rm -f .settings/org.eclipse.cdt.core.prefs

55a25e99ea3e93d4f91a8e24eb92b932c1c62146
Ilya Mizus <bigfatbrowncat@gmail.com>
2013-09-16 00:18:23 +0400

    New Avian version for OS X

--- bellardpi/src/cpp/gltest_conf.cpp
+++ bellardpi/src/cpp/gltest_conf.cpp
@@ -1,10 +1,1 @@
-#include <jni.h>
-#include <math.h>
-
 char* applicationClass = const_cast<char*>("bellardpi/Main");
-
-extern "C" JNIEXPORT jdouble JNICALL
-Java_java_lang_Math_log(JNIEnv*, jclass, jdouble val)
-{
-  return log(val);
-}

990d3badb5aa97abce0ad2091547de4ccedb78dc
Ilya Mizus <bigfatbrowncat@gmail.com>
2013-09-16 16:57:41 +0400

    Extracted ideconf to common-scripts. Added ide-conf support to bellardpi

---
+++ bellardpi/src/cpp/zetes_conf.cpp
@@ -0,0 +1,4 @@
+/**
+ * This constant is used by Zetes to find the Java code entry point
+ */
+char* applicationClass = const_cast<char*>("bellardpi/Main");
--- common-scripts/ideconf/ideconf.mk
+++ common-scripts/ideconf/ideconf.mk
@@ -1,4 +1,6 @@
-IDE_CONF=ideconf
+ifndef IDE_CONF_PATH
+  IDE_CONF_PATH=ideconf
+endif
 
 UNAME := $(shell uname)
 ifndef ARCH
@@ -14,25 +16,24 @@ else ifeq ($(UNAME) $(ARCH), Linux x86_64)	# linux on PC
 else ifeq ($(UNAME) $(ARCH), Linux armv6l)	# linux on Raspberry Pi
   PLATFORM_SHORT = linux
   PLATFORM_TAG = linux-armv6l
-else ifeq ($(OS) $(ARCH), Windows_NT i686)	# Windows
+else ifeq ($(OS) $(ARCH), Windows_NT i686)	# Windows x86
   PLATFORM_SHORT = win
   PLATFORM_TAG = win-i386
-else ifeq ($(OS) $(ARCH), Windows_NT x86_64)	# Windows
+else ifeq ($(OS) $(ARCH), Windows_NT x86_64)	# Windows x86_64
   PLATFORM_SHORT = win
   PLATFORM_TAG = win-x86_64
 endif
 
 VARS_SUBST='s|<ARCH>|$(ARCH)|g;s|<PLATFORM_SHORT>|$(PLATFORM_SHORT)|g;s|<JAVA_HOME>|$(JAVA_HOME)|g'
-IDE_CONF_SRC=$(IDE_CONF)/$(PLATFORM_TAG)
+IDE_CONF_SRC=$(IDE_CONF_PATH)/$(PLATFORM_TAG)
 
 ideconf-eclipse:
 	@echo Extracting Eclipse project files for $(PLATFORM_TAG)...
 	mkdir -p .settings
 	rm -f .settings/org.eclipse.cdt.core.prefs
-	sed $(VARS_SUBST) < $(IDE_CONF)/eclipse/settings/org.eclipse.cdt.core.prefs.template > .settings/org.eclipse.cdt.core.prefs
-	sed $(VARS_SUBST) < $(IDE_CONF)/eclipse/classpath.template > .classpath
-	sed $(VARS_SUBST) < $(IDE_CONF)/eclipse/project.template > .project
-	sed $(VARS_SUBST) < $(IDE_CONF)/eclipse/$(PLATFORM_SHORT)/cproject.template > .cproject
+	sed $(VARS_SUBST) < $(IDE_CONF_PATH)/eclipse/settings/org.eclipse.cdt.core.prefs.template > .settings/org.eclipse.cdt.core.prefs
+	sed $(VARS_SUBST) < $(IDE_CONF_PATH)/eclipse/classpath.template > .classpath
+	sed $(VARS_SUBST) < $(IDE_CONF_PATH)/eclipse/project.template > .project
+	sed $(VARS_SUBST) < $(IDE_CONF_PATH)/eclipse/$(PLATFORM_SHORT)/cproject.template > .cproject
 
-
-#.SILENT:
+.SILENT:

a0877519f8eeb39f1fb4abf85bd575565a346e9d
Ilya Mizus <bigfatbrowncat@gmail.com>
2013-10-09 18:28:32 +0400

    fix

---
+++ zeteshands/include/ZetesHands.h
@@ -0,0 +1,11 @@
+namespace zetes
+{
+	namespace hands
+	{
+		/**
+		 * This constant is used by Zetes to find the Java code entry point.
+		 * It should be defined in the user appolication
+		 */
+		extern char* applicationClass;
+	}
+}

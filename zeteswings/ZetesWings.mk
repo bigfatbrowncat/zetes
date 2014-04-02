ca71b1a0e3530a7715216b3bd4e8d2ab340c3300
Ilya Mizus <bigfatbrowncat@gmail.com>
2014-04-03 03:45:52 +0400

    Fix

--- zeteswings/ZetesWings.mk
+++ zeteswings/ZetesWings.mk
@@ -228,10 +228,7 @@ $(BINARY_PATH)/$(BINARY_NAME): $(JAVA_OBJECTS_PATH)/boot.jar $(ZETES_WINGS_PATH)
 	g++ $(RDYNAMIC) $(DEBUG_OPTIMIZE) -Llib/$(PLATFORM_TAG) $(CPP_OBJECTS) \
 	           @$(OBJECTS_PATH)/libzetesfeet/liblistpath.txt \
 	           @$(OBJECTS_PATH)/libzeteswings/liblistpath.txt \
-<<<<<<< HEAD
-=======
 	           @$(OBJECTS_PATH)/liblistpath.txt \
->>>>>>> 6114c2a... Build fix
 	           $(OBJECTS_PATH)/boot.jar.o \
 	           $(OBJECTS_PATH)/entry.str.o \
 	           $(PLATFORM_GENERAL_LINKER_OPTIONS) $(PLATFORM_CONSOLE_OPTION) -lm -lz -o $@
@@ -244,10 +241,7 @@ $(BINARY_PATH)/$(BINARY_NAME).debug$(SH_LIB_EXT): $(BINARY_PATH)/$(BINARY_NAME)
 	g++ -shared $(RDYNAMIC) $(DEBUG_OPTIMIZE) -Llib/$(PLATFORM_TAG) $(CPP_OBJECTS) \
 	           @$(OBJECTS_PATH)/libzetesfeet/liblistpath.txt \
 	           @$(OBJECTS_PATH)/libzeteswings/liblistpath.txt \
-<<<<<<< HEAD
-=======
 	           @$(OBJECTS_PATH)/liblistpath.txt \
->>>>>>> 6114c2a... Build fix
 	           $(OBJECTS_PATH)/boot.jar.o \
 	           $(OBJECTS_PATH)/entry.str.o \
 	           $(PLATFORM_GENERAL_LINKER_OPTIONS) $(PLATFORM_CONSOLE_OPTION) -lm -lz -o $@

9fcdd1700f1b01eaffc8076cd8a021f65a442b15
Ilya Mizus <bigfatbrowncat@gmail.com>
2014-05-07 04:53:24 +0400

    Added locateResource java wrapper to WinLinMacApi

--- zeteswings/src/cpp/WinLinMacApi_jni.cpp
+++ zeteswings/src/cpp/WinLinMacApi_jni.cpp
@@ -13,4 +13,17 @@ extern "C"
 		 jstring res = env->NewStringUTF(s.c_str());
 		 return res;
 	}
+	
+	JNIEXPORT jstring JNICALL Java_zetes_WinLinMacApi_locateResource(JNIEnv * env, jclass appClass, jstring path, jstring filename)
+	{
+		const char* pathStr = env->GetStringUTFChars(path, 0);
+		const char* filenameStr = env->GetStringUTFChars(filename, 0);
+
+		string res = WinLinMacApi::locateResource(string(pathStr), string(filenameStr));
+		
+		env->ReleaseStringUTFChars(path, pathStr);
+		env->ReleaseStringUTFChars(filename, filenameStr);
+
+		return env->NewStringUTF(res.c_str());
+	}
 }
--- zeteswings/src/java/zetes/WinLinMacApi.java
+++ zeteswings/src/java/zetes/WinLinMacApi.java
@@ -3,5 +3,5 @@ package zetes;
 public class WinLinMacApi {
 
 	public static native String locateExecutable();
-
+	public static native String locateResource(String path, String filename);
 }

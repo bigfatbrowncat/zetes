c9deb6f824dbb0840b362b54a33c029d65e7dbf9
Ilya Mizus <bigfatbrowncat@gmail.com>
2014-07-30 13:39:17 +0400

    MacOSX build fixed

--- common-scripts/globals.mk
+++ common-scripts/globals.mk
@@ -14,31 +14,50 @@ endif
 
 ifeq ($(UNAME), Darwin)	# OS X
   JAVA_HOME=$(shell /usr/libexec/java_home)
+  PLATFORM=darwin
   PLATFORM_TAG = darwin-x86_64
   JNILIB_EXT=.jnilib
   EXE_EXT=
   PIC=
 else ifeq ($(UNAME) $(ARCH), Linux x86_64)		# linux on PC
+  PLATFORM=linux
   PLATFORM_TAG = linux-x86_64
   JNILIB_EXT=.so
   EXE_EXT=
   PIC=-fPIC
 else ifeq ($(UNAME) $(ARCH), Linux armv6l)		# linux on Raspberry Pi
+  PLATFORM=linux
   PLATFORM_TAG = linux-armv6l
   JNILIB_EXT=.so
   EXE_EXT=
   PIC=-fPIC
 else ifeq ($(OS) $(ARCH), Windows_NT i686)		# Windows 32
+  PLATFORM=windows
   PLATFORM_TAG = windows-i386
   JNILIB_EXT=.dll
   EXE_EXT=.exe
   PIC=
 else ifeq ($(OS) $(ARCH), Windows_NT x86_64)	# Windows 64
+  PLATFORM=windows
   PLATFORM_TAG = windows-x86_64
   JNILIB_EXT=.dll
   EXE_EXT=.exe
   PIC=
 endif
+
+ifeq ($(CLASSPATH), android)
+  AVIAN_PLATFORM_SUFFIX = -android
+else
+  AVIAN_PLATFORM_SUFFIX =
+endif
+
+# In Avian Darwin is now macosx or ios. We use macosx
+ifeq ($(UNAME), Darwin)
+  AVIAN_PLATFORM_TAG = macosx-x86_64$(AVIAN_PLATFORM_SUFFIX)
+else
+  AVIAN_PLATFORM_TAG = $(PLATFORM_TAG)$(AVIAN_PLATFORM_SUFFIX)
+endif
+
 
 ifndef TARGET
   TARGET = target-$(PLATFORM_TAG)-$(CLASSPATH)
